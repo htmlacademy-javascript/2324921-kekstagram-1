64e46a54095c8f0f7c94738673473b6af77c1130
baranovdmitriy87 <147241780+baranovdmitriy87@users.noreply.github.com>
2024-06-30 22:52:36 +0300

    Добавляет в новую ветку код для 4-го ДЗ

--- js/functions.js
+++ js/functions.js
@@ -1,119 +1,24 @@
-// // // Пример
-// // Строка является палиндромом
-// имяФункции('топот'); // true
-// // Несмотря на разный регистр, тоже палиндром
-// имяФункции('ДовОд'); // true
-// // Это не палиндром
-// имяФункции('Кекс');  // false
+// // Добавочный символ использован один раз
+// имяФункции('1', 2, '0');      // '01'
 
+// // Добавочный символ использован три раза
+// имяФункции('1', 4, '0');      // '0001'
 
-//                     ====== Полиндром =======
+// // Добавочные символы обрезаны с конца
+// имяФункции('q', 4, 'werty');  // 'werq'
 
-// //   -- 1-й Вариант. --
-// const isPalindrome = (string) => {
-//   const tempString = string
-//     .toLowerCase()
-//     .toUpperCase()
-//     .replaceAll('', ' ');
-//   let reverseString = '';
-//   for (let i = tempString.length - 1; i >= 0; i--) {
-//     reverseString += tempString.at(i);
-//   }
-//   return reverseString === tempString;
-// };
+// // Добавочные символы использованы полтора раза
+// имяФункции('q', 4, 'we');     // 'wweq'
 
-// console.log(isPalindrome('Довд'));
+// // Добавочные символы не использованы, исходная строка не изменена
+// имяФункции('qwerty', 4, '0'); // 'qwerty'
 
-// isPalindrome('КоМк')
+const myPadStart = (string, minLength, pad) => {
+  const actualPad = minLength - string.length;
 
-// -- 2-й Вариант. --
-const isPalindrome = (string) => {
-  const tempString = string
-    .toLowerCase()
-    .toUpperCase()
-    .replaceAll(' ', '');
-  const reverseString = tempString.split('').reverse().join('');
-  return reverseString === tempString;
-};
-
-isPalindrome('КоМк');
-
-// ============   Функция для проверки длины строки    ==============
-
-// Cтрока короче 20 символов
-// имяФункции('проверяемая строка', 20); // true
-// Длина строки ровно 18 символов
-// имяФункции('проверяемая строка', 18); // true
-// Строка длиннее 10 символов
-// имяФункции('проверяемая строка', 10); // false
-
-
-// // -- 1-й Враиант. --
-// const isLessOrEqual = (string, length) => {
-//   if (string.length <= length) {
-//     return true;
-//   }
-//   return false;
-// };
-
-// isLessOrEqual('проверяемая строка', 18);
-
-
-// console.log(isLessOrEqual);
-// isLessOrEqual('проверяемая строка', 10);
-
-// -- 2-й Вариант. --
-// const checkStringLength = (string, length) => {
-//     return string.length <= length;
-// }
-// checkStringLength('проверяемая строка', 18);
-
-//  -- 3-й Вариант (Самый локаничный и читаемый) --
-const checkStringLength = (string, length) => string.length <= length;
-checkStringLength('проверяемая строка', 18);
-
-
-// ===============  Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9    ===============
-//  и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN:
-
-// имяФункции('2023 год');            // 2023
-// имяФункции('ECMAScript 2022');     // 2022
-// имяФункции('1 кефир, 0.5 батона'); // 105
-// имяФункции('агент 007');           // 7
-// имяФункции('а я томат');           // NaN
-
-
-// -- 1-й Вариант. --
-// const extractNumber = (string) => {
-//   if (typeof string === 'number') {
-//       return string;
-//   }
-//   let result = '';
-//   for (let i = 0; i < string.length; i++) {
-//       if (!Number.isNaN(parseInt(string.at(i), 10))) {
-//         result += string.at(i)
-//       }
-//   }
-//   return parseInt(result, 10);
-// }
-// console.log(extractNumber ('ECMAScript 2022'));
-
-//  -- 2-й Вариант. --
-// const extractNumber = (string) => {
-//   let result = '';
-//   for (let i = 0; i < string.length; i++) {
-//     if (!Number.isNaN(parseInt(string.at(i), 10))) {
-//       result += string.at(i);
-//     }
-//   }
-//   return parseInt(result, 10);
-// };
-// extractNumber('1 кефир, 0.5 батона');
-
-// -- 3-й Вариант --
-const extractNumber = (string) => {
-  const digits = string.match(/\d+/g);
-  return digits ? parseInt(digits.join(''), 10) : NaN;
-};
-
-extractNumber('1 кефир, 0.5 батона');
+  if (actualPad <= 0) {
+    return string;
+  }
+  return pad.slice (0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
+}
+console.log(myPadStart('1', 2, '0'));
