ee0a557d6654133e779f03eb42bb810e9c408294
Keks <keks@htmlacademy.ru>
2024-07-06 09:35:23 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,17 +1,3 @@
-import './util.js';
-import './data.js';
-// import './functions.js';
-
-import {createPicture, getPictures} from './generator-id/create-picture.js';
-getPictures();
-createPicture();
-
-import {createMessage, createComment} from './generator-id/comment-message.js';
-createMessage();
-createComment();
-
-import {createIdGenerator, generateCommentId} from './generator-id/create-generator-index.js';
-createIdGenerator();
-generateCommentId();
+import { getPictures } from './mocks/photo.js';
 
 getPictures();
