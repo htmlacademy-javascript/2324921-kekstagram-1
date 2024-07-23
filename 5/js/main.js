66ca0574e90230ece722d04ae4cdc506ca629acf
Keks <keks@htmlacademy.ru>
2024-07-23 09:48:54 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,4 @@
+import { getPictures } from './mocks/photo.js';
+import { renderThumbnails } from './thumbnail.js';
+
+renderThumbnails(getPictures());
---
+++ 5/js/mocks/photo.js
@@ -0,0 +1,21 @@
+import { getRandomArrayElements, getRandomInteger } from '../util.js';
+import { DESCRIPTIONS, LIKE_MIN_COUNT, LIKE_MAX_COUNT, COMMENT_COUNT, PICTURE_COUNT } from './const.js';
+import { createComment } from './comment.js';
+
+const createPicture = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: getRandomArrayElements(DESCRIPTIONS),
+  likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
+  comment: Array.from(
+    { length: getRandomInteger(0, COMMENT_COUNT) },
+    createComment
+  ),
+});
+
+const getPictures = () =>
+  Array.from({ length: PICTURE_COUNT }, (_, pictureIndex) =>
+    createPicture(pictureIndex + 1)
+  );
+
+export { getPictures };
