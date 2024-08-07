b76925b535d22ef5a613c01e12df5c609e6bc7c3
Keks <keks@htmlacademy.ru>
2024-08-07 08:53:33 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/big-picture.js
+++ 6/js/big-picture.js
@@ -1,13 +1,41 @@
 import { isEscapeKey } from './util.js';
 
 // const COMMENTS_PER_PORTION = 5;
-// const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 const body = document.querySelector('body');
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureDetails = bigPicture.querySelector('.big-picture__img img');
 const bigPictureLikes = bigPicture.querySelector('.likes-count');
 const bigPictureSocialCaption = bigPicture.querySelector('.social__caption');
 const cancelButton = bigPicture.querySelector('.big-picture__cancel');
+const commentCount = bigPicture.querySelector('.social__comment-count');
+const commentList = bigPicture.querySelector('.social__comments');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+
+
+// let commentsShown = 0;
+let comments = [];
+
+const createComments = ({ avatar, name, message }) => {
+  const comment = commentTemplate.cloneNode(true);
+  const commentPicture = comment.querySelector('.social__picture');
+  commentPicture.src = avatar;
+  commentPicture.alt = name;
+  comment.querySelector('.social__text').textContent = message;
+
+  return comment;
+};
+
+const renderComments = () => {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < comments.length; i++) {
+    const commentElement = createComments(comments[i]);
+    fragment.append(commentElement);
+  }
+  // commentList.innerHTML = '';
+  commentList.append(fragment);
+  // commentCount.innerHTML = '${commentsShown}'; /** Добавить еще...но не совсем пойму что конкретно и для чего  */
+};
 
 const hideBigPicture = () => {
   bigPicture.classList.add('hidden');
@@ -36,10 +64,14 @@ const renderPictureDetails = ({ url, likes, description }) => {
 const showBigPicture = (data) => {
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
+  commentsLoader.classList.add('hidden');
+  commentCount.classList.add('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
 
   renderPictureDetails(data);
-  renderComments(data.comments);
+  commentList.innerHTML = '';
+  comments = data.comments;
+  renderComments();
 };
 
 cancelButton.addEventListener('click', onCancelButtonClick);
