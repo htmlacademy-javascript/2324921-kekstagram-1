f0a16ba69b33362584b6e81e0b35114a4b85455f
baranovdmitriy87 <147241780+baranovdmitriy87@users.noreply.github.com>
2024-08-21 23:13:27 +0300

    Продолжает писать код для проверки формы

--- js/user-form.js
+++ js/user-form.js
@@ -4,30 +4,78 @@ const TAG_ERROR_TEXT = 'Заполненное поле неверно';
 
 const form = document.querySelector('.img-upload__form');
 const fileField = document.querySelector('#upload-file');
-const Overlay = form.querySelector('.img-upload__overlay');
+const overlay = form.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
 const cancelButton = document.querySelector('#upload-cancel');
 const hashtagField = document.querySelector('.text__hashtags');
 const commentField = document.querySelector('.text__description');
 
-imgUploadForm.addEventListener('submit', () => {
-  uploadOverlay.classList.remove('hidden');
+const showModal = () => {
+  overlay.classList.remove('hidden');
   body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const hideModal = () => {
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'fimg-upload__field-wrapper--error'
 });
 
-const pristine = new Pristine(imgUploadForm, {
-  classTo: 'img-upload__form',
-  errorClass: 'img-upload__form--invalid',
-  successClass: 'img-upload__form--valid',
-  errorTextParent: 'img-upload__form',
-  errorTextTag: 'span',
-  errorTextClass: 'fimg-upload__form-error'
-});
+const isTextFieldFocused = () => {
+  document.activeElement === hashtagField || document.activeElement === commentField;
+};
 
-pristine.addValidator(imgUploadForm.querySelector('#nickname'), validateNickname);
+function onDocumentKeydown(evt) {
+  if (evt.key === 'Escape' && !isTextFieldFocused()) {
+    evt.preventDefault();
+    hideModal();
+  }
+};
+
+const onCancelButtonClick = () => {
+  hideModal();
+};
+
+const onFileFieldChange = () => {
+  showModal();
+};
+
+// Проверка валидации хештега по написания и кол-ву символов
+const isValidTag = (tag) => VALID_SYMBOLS.test(tag);
+const hasValidCount = (tags) => tags.lenght <= MAX_HASHTAG_COUNT;
+
+const hasUniqueTags = (tags) => {
+  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+  return lowerCaseTags.lenght === new Set(lowerCaseTags).size;
+};
+
+const validateTags = (value) => {
+  const tags = value
+    .trim()
+    .split(' ')
+    .filter((tag) => tag.trim().length);
+  return hasValidCount(tags) && hasUniqueTags(tags) && tags.every(isValidTag);
+};
+
+pristine.addValidator(
+  hashtagField,
+  validateTags,
+  TAG_ERROR_TEXT
+);
 
 // Проверка валидации формы
-imgUploadForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
+const onFormSubmit = (evt) => {
+ evt.preventDefault();
   pristine.validate();
-});
+};
+
+fileField.addEventListener('change', onFileFieldChange);
+cancelButton.addEventListener('click', onCancelButtonClick);
+form.addEventListener('submit', onFormSubmit);
