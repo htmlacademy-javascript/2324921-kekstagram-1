583275d804773a2eabac6cd49283a98fd00de042
baranovdmitriy87 <147241780+baranovdmitriy87@users.noreply.github.com>
2024-08-26 22:52:52 +0300

    Добавляет новый модуль для эфффектов слайдера

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
 import { resetScale } from './scale.js';
+import { resetEffects } from './effect.js';
 
 const MAX_HASHTAG_COUNT = 5;
 const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
@@ -28,6 +29,7 @@ const hideModal = () => {
   form.reset();
   pristine.reset();
   resetScale();
+  resetEffects();
   overlay.classList.add('hidden');
   body.classList.remove('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
