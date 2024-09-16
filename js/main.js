2634b04b07470d30897f1336c63f7830179e3f3f
baranovdmitriy87 <147241780+baranovdmitriy87@users.noreply.github.com>
2024-09-16 15:52:47 +0300

    Добавляет модуль для фильтрации

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ import { renderGallery } from './gallery.js';
 import { setOnFormSubmit, hideModal } from './form.js';
 import { showSuccessMessage, showErrorMessage } from './message.js';
 import { getData, sendData } from './api.js';
-import { showAlert } from './util.js';
+import { showAlert, debounce } from './util.js';
 import './form.js';
+import { init, getFilteredPictures } from './filter.js';
 
 setOnFormSubmit(async (data) => {
   try {
@@ -17,7 +18,9 @@ setOnFormSubmit(async (data) => {
 
 try {
   const data = await getData();
-  renderGallery(data);
+  const debouncedRenderGallery = debounce(renderGallery);
+  init(data, debouncedRenderGallery);
+  renderGallery(getFilteredPictures);
 } catch (err) {
   showAlert(err.message);
 }
--- js/util.js
+++ js/util.js
@@ -38,4 +38,29 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-export { getRandomInteger, getRandomArrayElements, isEscapeKey, imageElement, showAlert };
+function debounce(callback, timeoutDelay = 500) {
+
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+// function throttle(callback, delayBetweenFrames) {
+
+//   let lastTime = 0;
+
+//   return (...rest) => {
+
+//     const now = new Date();
+
+//     if (now - lastTime >= delayBetweenFrames) {
+//       callback.apply(this, rest);
+//       lastTime = now;
+//     }
+//   };
+// }
+
+export { getRandomInteger, getRandomArrayElements, isEscapeKey, imageElement, showAlert, debounce };
